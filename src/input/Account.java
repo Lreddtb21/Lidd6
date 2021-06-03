2c28ab03eead0af5d587f099266407a7fe051e68
djack11 <djack11@hotmail.com>
2021-06-03 16:27:19 -0400

    d 1st commit

--- src/input/Account.java
+++ src/input/Account.java
@@ -53,7 +53,7 @@ public class Account {
 			System.out.println("Welcome, " + customerName + "!");
 			System.out.println();
 			System.out.println("A. Your Previous Weight");
-			System.out.println("B. Add Weight");
+			//System.out.println("B. Add Weight");
 			System.out.println("C. Subtract Weight");
 			System.out.println("D. Show Previous Weigh In");
 			System.out.println("E. Finished");
@@ -61,12 +61,52 @@ public class Account {
 			do  { 
 				System.out.println();
 				System.out.println("Enter An Option");
+				char option1 = scanner.next().charAt(0);
+				option = Character.toUpperCase(option1);
+				System.out.println();
+				
+			switch(option) { 
+			//case A to check current weight
+			case 'A':
+				System.out.println("********************");
+				System.out.println("currentWeight = lbs " + currentWeight);
+				System.out.println("********************");
+				break;
+				//case B to add weight
+				//System.out.println("Enter New Weight Added: ");
+				//float amount = scanner.nextFloat();
+				//addWeight (amount);
+				//System.out.println();
+				//break;
+				//case C subtract weight
+			case 'C':
+				System.out.println("Enter New Weight Subtracted: ");
+				float amount2 = scanner.nextFloat();
+				subtractWeight(amount2);
+				System.out.println();
+				break;
+				//case D view last weigh in
+			case 'D':
+				System.out.println("********************");
+				getLastWeighIn();
+				System.out.println("********************");
+				System.out.println();
+				break;
+				//case E exits program
+			case 'E':
+				System.out.println("********************");
+				break;
+				//invalid input
+				default:
+					System.out.println("Invalid. Please Enter Either A, B, C, D");
+					break;
+					
+			}
+			} while(option != 'E');
+			System.out.println("Keep Up The Great Work!");
 			}
 		
 			
-		}
-	
-   
-	}
+}
 
 
